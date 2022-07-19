e1756b2c7e95eaaf771f31b0c00490dc3c6c524d
Yoshinori-Fukusumi <cakdq701@gmail.com>
2022-07-19 14:03:20 +0900

    制御構文2の課題(1,2修正)

--- 05/01_if2.php
+++ 05/01_if2.php
@@ -4,8 +4,8 @@ $poket_money = 1000;
 $fund_raising = 101;
 
 // ここに処理を記述
+echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
 while ($poket_money >= $fund_raising) {
-    echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
     $poket_money = $poket_money - $fund_raising;
     echo "{$fund_raising}円募金しました｡" . PHP_EOL;
     echo "残り残高は{$poket_money}円です。" . PHP_EOL;
--- 05/02_if2.php
+++ 05/02_if2.php
@@ -4,10 +4,10 @@ $poket_money = 1000;
 $fund_raising = 101;
 
 // ここに処理を記述
-do{
-    echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
+echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
+do {
     $poket_money = $poket_money - $fund_raising;
     echo "{$fund_raising}円募金しました｡" . PHP_EOL;
     echo "残り残高は{$poket_money}円です。" . PHP_EOL;
-}while ($poket_money >= $fund_raising);
+} while ($poket_money >= $fund_raising);
 echo "あなたはこれ以上募金できません｡";
