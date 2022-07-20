dbeadb5bc16a48938931ce77037b1759e0d90e48
Yoshinori-Fukusumi <cakdq701@gmail.com>
2022-07-20 13:25:25 +0900

    フォームの課題(戻るボタン追加)

--- 06/02_result.php
+++ 06/02_result.php
@@ -5,3 +5,19 @@ $msg = $_POST['message'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo "私の名前は、" . htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') . "です。";
 }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>フォーム課題2</title>
+</head>
+
+<body>
+    <br>
+    <br>
+    <a href="http://localhost/06/02_form.php">戻る</a>
+</body>
+
+</html>
