3268c6d711470d72e4623d8410530f1e2c9b1d6c
Yoshinori-Fukusumi <cakdq701@gmail.com>
2022-07-28 19:53:23 +0900

    function_4修正

--- 08/function_4.php
+++ 08/function_4.php
@@ -6,9 +6,7 @@ function check_temperature($body_temperature)
     // 37度未満の場合はtrue
     // 37度以上の場合はfalse
     if ($body_temperature < 37) {
-        return 'true';
-    } elseif ($body_temperature >= 37) {
-        return 'false';
+        return true;
     }
 }
 
@@ -18,9 +16,9 @@ function create_message($body_temperature)
     // check_temperature関数を呼び出す
     // check_temperature関数の結果によって文字列を変更し、関数の戻り値として設定
 
-    if (check_temperature($body_temperature) == 'true') {
+    if (check_temperature($body_temperature)) {
         return 'あなたは平熱なので、問題なく参加できます';
-    } elseif (check_temperature($body_temperature) == 'false') {
+    } else {
         return 'あなたは発熱なので、参加できません';
     }
 }
